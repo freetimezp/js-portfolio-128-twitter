ba0d026234288b9b6ae8509158dbe8682fbb9d05
freetimezp <kraftukrnet@ukr.net>
2024-10-14 21:08:32 +0300

    10. js-portfolio-128-twitter. Func updateUser User controller.

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,12 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getUserProfile, followUnfollowUser, getSuggestedUsers } from "../controllers/user.controller.js";
+import {
+    getUserProfile,
+    followUnfollowUser,
+    getSuggestedUsers,
+    updateUser
+} from "../controllers/user.controller.js";
 
 
 const router = express.Router();
@@ -10,7 +15,7 @@ const router = express.Router();
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUser);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import { v2 as cloudinary } from "cloudinary";
+
 //import routes
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -10,12 +12,20 @@ import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 
 //endpoints
 app.use("/api/auth", authRoutes);
