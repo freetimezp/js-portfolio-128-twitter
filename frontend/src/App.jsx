0e4e2b47ca7f331232d1e629abb718e802230428
freetimezp <kraftukrnet@ukr.net>
2024-10-17 18:42:56 +0300

    25. js-portfolio-128-twitter. Sidebar component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignupPage from "./pages/auth/signup/SignupPage";
 import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
 
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
