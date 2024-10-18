23a762a53920d1024bc5866792b7891857544060
freetimezp <kraftukrnet@ukr.net>
2024-10-18 22:03:48 +0300

    37. js-portfolio-128-twitter. Display posts from mongo db. Home page.

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -42,7 +42,7 @@ const Post = ({ post }) => {
                 <div className='avatar'>
                     <Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full 
                         overflow-hidden'>
-                        <img src={postOwner.profileImg || "/avatar-placeholder.png"} />
+                        <img src={postOwner.profileImg || "/avatars/avatar-placeholder.png"} />
                     </Link>
                 </div>
                 <div className='flex flex-col flex-1'>
--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,27 +1,62 @@
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 
-import { POSTS } from "../../utils/db/dummy";
+const Posts = ({ feedType }) => {
+    const getPostEndpoint = () => {
+        switch (feedType) {
+            case 'forYou':
+                return '/api/posts/all';
 
-const Posts = () => {
-    let isLoading = false;
+            case 'following':
+                return '/api/posts/following';
+
+            default:
+                return '/api/posts/all';
+        }
+    };
+
+    const POST_ENDPOINT = getPostEndpoint();
+
+    const { data: posts, isLoading, refetch, isRefetching } = useQuery({
+        queryKey: ['posts'],
+        queryFn: async () => {
+            try {
+                const res = await fetch(POST_ENDPOINT);
+                const data = await res.json();
+
+                if (!res.ok) throw new Error(data.error || "Something went wrong with getting posts!");
+
+                return data;
+
+            } catch (error) {
+                console.log(error);
+                throw new Error(error);
+            }
+        }
+    });
+
+    useEffect(() => {
+        refetch();
+    }, [feedType, refetch]);
 
     return (
         <>
-            {(isLoading) && (
+            {(isLoading || isRefetching) && (
                 <div className='flex flex-col justify-center'>
                     <PostSkeleton />
                     <PostSkeleton />
                     <PostSkeleton />
                 </div>
             )}
-            {!isLoading && POSTS?.length === 0 && (
+            {!isLoading && !isRefetching && posts?.length === 0 && (
                 <p className='text-center my-4'>No posts in this tab. Switch 👻</p>
             )}
-            {!isLoading && POSTS && (
+            {!isLoading && !isRefetching && posts && (
                 <div>
-                    {POSTS.map((post) => (
+                    {posts.map((post) => (
                         <Post key={post._id} post={post} />
                     ))}
                 </div>
