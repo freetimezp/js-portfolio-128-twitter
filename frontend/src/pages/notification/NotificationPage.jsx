c9dac6dd330fe60318934751fbba3df672b89d41
freetimezp <kraftukrnet@ukr.net>
2024-10-20 00:08:27 +0300

    44. js-portfolio-128-twitter. Display notifications.

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,39 +1,65 @@
 import { Link } from "react-router-dom";
 
-import LoadingSpinner from "../../components/common/LoadingSpinner";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { toast } from "react-hot-toast";
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
+import LoadingSpinner from "../../components/common/LoadingSpinner";
+
 const NotificationPage = () => {
+    const queryClient = useQueryClient();
 
-    let isLoading = false;
+    const { data: notifications, isLoading } = useQuery({
+        queryKey: ["notifications"],
+        queryFn: async () => {
+            try {
+                const res = await fetch(`/api/notifications`);
 
-    const deleteNotifications = (e) => {
-        e.preventDefault();
-    };
+                const data = await res.json();
 
-    const notifications = [
-        {
-            _id: "1",
-            from: {
-                _id: "1",
-                username: "Johndoe",
-                profileImg: "/avatars/boy2.jpg"
-            },
-            type: "follow"
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong");
+                }
+                console.log(data);
+                return data;
+            } catch (error) {
+                throw new Error(error.message);
+            }
+        }
+    });
+
+
+    const { mutate: deleteNotifications } = useMutation({
+        mutationFn: async () => {
+            try {
+                const res = await fetch(`/api/notifications/`, {
+                    method: "DELETE"
+                });
+
+                const data = await res.json();
+
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong");
+                }
+
+                return data;
+
+            } catch (error) {
+                throw new Error(error);
+            }
         },
-        {
-            _id: "2",
-            from: {
-                _id: "2",
-                username: "Janedoe",
-                profileImg: "/avatars/girl1.jpg"
-            },
-            type: "like"
+        onSuccess: () => {
+            toast.success("Notifications deleted successfully");
+            queryClient.invalidateQueries({ queryKey: ["notifications"] });
         },
-    ];
+        onError: (error) => {
+            toast.error(error.message);
+        }
+    });
 
     return (
         <>
@@ -54,21 +80,28 @@ const NotificationPage = () => {
                         </ul>
                     </div>
                 </div>
+
                 {isLoading && (
                     <div className='flex justify-center h-full items-center'>
                         <LoadingSpinner size='lg' />
                     </div>
                 )}
-                {notifications?.length === 0 && <div className='text-center p-4 font-bold'>No notifications 🤔</div>}
+
+                {notifications?.length === 0 && (
+                    <div className='text-center p-4 font-bold'>No notifications 🤔</div>
+                )}
+
                 {notifications?.map((notification) => (
                     <div className='border-b border-gray-700' key={notification._id}>
                         <div className='flex gap-2 p-4'>
                             {notification.type === "follow" && <FaUser className='w-7 h-7 text-primary' />}
+
                             {notification.type === "like" && <FaHeart className='w-7 h-7 text-red-500' />}
+
                             <Link to={`/profile/${notification.from.username}`}>
                                 <div className='avatar'>
                                     <div className='w-8 rounded-full'>
-                                        <img src={notification.from.profileImg || "/avatar-placeholder.png"} />
+                                        <img src={notification.from.profileImg || "/avatars/avatar-placeholder.png"} />
                                     </div>
                                 </div>
                                 <div className='flex gap-1'>
